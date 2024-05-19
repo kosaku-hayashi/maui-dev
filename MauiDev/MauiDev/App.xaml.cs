71bcf1bd6092cace80f9904bdd652a31144cb8e1
kosaku-hayashi <hayasi.mkmk128@gmail.com>
2024-05-19 23:40:00 +0900

    Add MauiIcons and CommunityToolkit.Maui

---
+++ MauiDev/MauiDev/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiDev;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MauiDev/MauiDev/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using MauiIcons.FontAwesome;
+using MauiIcons.Material;
+
+namespace MauiDev;
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMaterialMauiIcons()
+            .UseFontAwesomeMauiIcons()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
